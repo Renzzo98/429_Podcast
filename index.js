4282d73c88130b42ebde67fc7708352b73720318
Hugo Renzzo Olcese <renzzo98@yahoo.com>
2020-07-24 00:35:51 -0400

    added https middleware

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
 
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 /**
  * App Variables
@@ -13,6 +16,14 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "80";
+
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+
 
 /**
  *  App Configuration
@@ -39,6 +50,15 @@ app.get("/about", (req, res) => {
  * Server Activation
  */
 
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
+ // Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
